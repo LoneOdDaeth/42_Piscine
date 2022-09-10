54e59da7a3c18cc4aca1e79b0e21a435a4129015
Selim Oguz Sahin <selsahin@k1m21s04.42istanbul.com.tr>
2022-09-10 16:37:34 +0300

    Succes: %85

---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: selsahin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/24 15:46:02 by selsahin          #+#    #+#             */
+/*   Updated: 2022/08/24 15:56:30 by selsahin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	c;
+
+	c = 'z';
+	while (c >= 'a')
+	{
+		write (1, &c, 1);
+		c--;
+	}
+}
+/*
+int	main()
+{
+	ft_print_reverse_alphabet();
+	return (0);
+}
+*/
